d51db7d2cbbefec048cec3676f8e08edc724e8f7
minall <minhminhpy18042001@gmail.com>
2024-10-07 20:50:39 +0700

    toast

---
+++ frontend/src/components/Toast.tsx
@@ -0,0 +1,23 @@
+type ToastProps ={
+    message:string;
+    type:"SUCCESS"|"ERROR"
+};
+
+const Toast =({message,type}:ToastProps )=>{
+
+    
+
+    const styles=type ==="SUCCESS"
+     ? "fixed top-4 right-4 z-50 rounded-md bg-green-600 text-white max-w-md"
+     : "fixed top-4 right-4 z-50 rounded-md bg-red-600 text-white max-w-md"
+
+    return(
+        <div className={styles}>
+            <div className="flex justify-center items-center">
+                <span className="text-lg font-semibold">
+                    {message}
+                </span>
+            </div>
+        </div>
+    );
+};
