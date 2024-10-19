4cc2b28db8a474814931c4bc4ce9697e51c76328
minall <minhminhpy18042001@gmail.com>
2024-10-19 19:07:52 +0700

    add-hotel

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -13,39 +13,57 @@ const upload =multer({
         fileSize:5*1024*1024,
     }
 })
-router.post("/",verifyToken as any,[
+router.post(
+  "/",
+  verifyToken as any,
+  [
     body("name").notEmpty().withMessage("Name is required"),
     body("city").notEmpty().withMessage("City is required"),
     body("country").notEmpty().withMessage("Country is required"),
     body("description").notEmpty().withMessage("Description is required"),
     body("type").notEmpty().withMessage("Hotel type is required"),
-    body("pricePerNight").notEmpty().isNumeric().withMessage("Price per night is required and must be a number"),
-    body("facilities").notEmpty().isArray().withMessage("Facilities are required"),],
-    upload.array("imageFiles", 6), async (res: Response, req: Request) => {
+    body("pricePerNight")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Price per night is required and must be a number"),
+    body("facilities")
+      .notEmpty()
+      .isArray()
+      .withMessage("Facilities are required"),
+  ],
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
     try {
-        const imageFiles =req.files as Express.Multer.File[];
-        const newHotel:HotelType =req.body;
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel: HotelType = req.body;
 
-        const uploadPromises=imageFiles.map(async(image)=>{
-            const b64 = Buffer.from(image.buffer).toString("base64");
-            let dataURI = "data:" + image.mimetype + ";base64," + b64;
-            const res = await cloudinary.v2.uploader.upload(dataURI);
-            return res.url;
-        });
-        const imageUrls=await Promise.all(uploadPromises);
-        newHotel.imageUrls=imageUrls;
-        newHotel.lastUpdated=new Date()
-        newHotel.userId=req.userId;
+      const imageUrls = await uploadImages(imageFiles);
 
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
 
-        const hotel= new Hotel(newHotel);
-        await hotel.save();
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
 
-        res.status(201).send(hotel);
-
-    } catch (error) {
-        console.log("Error creating hotel: ",error);
-        res.status(500).json({message:"Some thing went wrong"})
+      res.status(201).send(hotel);
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: "Something went wrong" });
     }
-});
+  }
+);
+
+async function uploadImages(imageFiles: Express.Multer.File[]) {
+    const uploadPromises = imageFiles.map(async (image) => {
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        let dataURI = "data:" + image.mimetype + ";base64," + b64;
+        const res = await cloudinary.v2.uploader.upload(dataURI);
+        return res.url;
+    });
+
+    const imageUrls = await Promise.all(uploadPromises);
+    return imageUrls;
+}
+
 export default router
