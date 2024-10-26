5123a88a1fc19c854ddfa6fdd48f62f37a69ef05
minall <minhminhpy18042001@gmail.com>
2024-10-27 00:03:57 +0700

    filter and sort

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -115,6 +115,11 @@ export type SearchParams ={
   adultCount?:string;
   childCount?:string;
   page?:string;
+  facilities?: string[];
+  types?: string[];
+  stars?: string[];
+  maxPrice?: string;
+  sortOption?: string;
 }
 export const searchHotels =async(searchParams:SearchParams):Promise<HotelSearchResponse>=>{
   const queryParams = new URLSearchParams();
@@ -124,7 +129,16 @@ export const searchHotels =async(searchParams:SearchParams):Promise<HotelSearchR
   queryParams.append("adultCount", searchParams.adultCount || "");
   queryParams.append("childCount", searchParams.childCount || "");
   queryParams.append("page", searchParams.page || "");
+  
+  queryParams.append("maxPrice", searchParams.maxPrice || "");
+  queryParams.append("sortOption", searchParams.sortOption || "");
 
+  searchParams.facilities?.forEach((facility) =>
+    queryParams.append("facilities", facility)
+  );
+
+  searchParams.types?.forEach((type) => queryParams.append("types", type));
+  searchParams.stars?.forEach((star) => queryParams.append("stars", star));
   const response = await fetch(`${API_BASE_URL}/api/hotels/search?${queryParams}`);
 
   if(!response.ok){
